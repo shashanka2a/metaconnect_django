20f319ff8b53a2404fcdb746385982dbbe83fd0f
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-03-13 19:33:41 +0530

    update
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- metaconnect/views.py
+++ metaconnect/views.py
@@ -49,10 +49,10 @@ def Dashboard(request,address):
 
     return render(request,'user_page.html',{'walletdetails':userwallet,'project':project})
     
-
 def upload(request,address):
     userwallet  = Owner.objects.get(owneraddress=address)
     project=ProjectDetails.objects.get(user=userwallet)
+
     path =str(project.image)
     file = open(path, "rb")
     print(file)
@@ -64,10 +64,64 @@ def upload(request,address):
     print('HWLLLLLLLLLLLLLLLLLLLL')
     print(type(response))
     x = response.json()
-    print(x['response'])
-    print(x['ipfs_url'])
-    print(x['file_name'])
+    contract  = getData()
+    contract=contract.replace('_newuri',"'"+x['ipfs_url']+"'")
+    contract=contract.replace('MyToken',project.name)
+
     print(x['error'])
     
+    
 
-    return render(request,'output.html',{'response':x['response'], 'ipfs_url':x['ipfs_url'],'file_name':x['file_name'],'error':x['error']}   )
+    return render(request,'output.html',{'contract':contract,'userwallet':userwallet,'project':project,'response':x['response'], 'ipfs_url':x['ipfs_url'],'file_name':x['file_name'],'error':x['error']}   )
+
+
+def deploy(request,address):
+    userwallet  = Owner.objects.get(owneraddress=address)
+    project=ProjectDetails.objects.get(user=userwallet)
+    url = "https://api.nftport.xyz/v0/contracts"
+    payload = "{\n  \"chain\": \"polygon\",\n  \"name\": \"CRYPTOPUNKS\",\n  \"symbol\": \"CYBER\",\n  \"owner_address\":coollll,\n  \"metadata_updatable\": false,\n  \"type\": \"erc1155\"\n}"
+    print(project.name)
+    print(project.symbol)
+    payload = payload.replace('CRYPTOPUNKS',project.name)
+    payload = payload.replace('CYBER',project.symbol)
+    payload=payload.replace('coollll','"'+address+'"')
+
+    headers = {
+    'Content-Type': "application/json",
+    'Authorization': "d775b8ce-a5b7-420b-b818-3e7c46075ab2"
+    }
+    response = requests.request("POST", url, data=payload, headers=headers)
+    print(response.text)
+    print('--------------------')
+    print(response)
+    return render(request,'deploy.html',{'respose':response.text})
+
+def getData():
+    return '''
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC1155, Ownable {
+    constructor() ERC1155("") {}
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(_newuri);
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+}'''
